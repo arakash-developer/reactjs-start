552368512102a06cfd5ad9e1901ec5e4d538dbd4
akash100a <arakash968786@gmail.com>
2024-11-23 15:36:48 +0600

    up

--- src/component/layers/Root.jsx
+++ src/component/layers/Root.jsx
@@ -2,16 +2,13 @@ import React, { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
-import Load from './Load'
 
 
 const Root = () => {
     return (
         <>
             <Navbar />
-            <Suspense fallback={<Load/>}>
                 <Outlet />
-            </Suspense>
             <Footer />
         </>
     )
