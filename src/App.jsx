5556f8e5c14d02b4dd54748efda9552b59c1dec7
akash100a <arakash968786@gmail.com>
2024-11-23 12:11:32 +0600

    up

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,15 @@ import {
 import './App.css'
 import Root from './component/layers/Root.jsx'
 import Error from './component/pages/Error';
+import Home from './component/pages/Home';
+import About from './component/pages/About';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Root />}>
-      {/* <Route path='/' element={<Home />}/>
+      <Route path='/' element={<Home />}/>
       <Route path='/home' element={<Home />}></Route>
-      <Route path='/shop' element={<Products />}></Route>
-      <Route path='/products' element={<Products />}></Route>
-      <Route path='/services' element={<Services />}></Route>
-      <Route path='/faq' element={<Faq />}></Route>
       <Route path='/about' element={<About />}></Route>
-      <Route path='/contact' element={<Contact />}></Route>
-      <Route path='/cart/:id' element={<Cart />}></Route>
-      <Route path='/lern' element={<Learn />}></Route>
-      <Route path='/products/details/:id' element={<Product />}></Route> */}
       <Route path='*' element={<Error />}/>
     </Route>
   )
